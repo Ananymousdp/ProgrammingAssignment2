640c0cbed934b1f44f35f54fdf0b315699eced98
Ananymousdp <38840906+Ananymousdp@users.noreply.github.com>
2018-11-20 22:40:28 +0530

    Update cachematrix.R
    Gives inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,30 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## It helps us to assign,get matrix and sets and gets inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv<-NULL
+  inv<-NULL ##Initializing inv to NULL
   set<-function(y){
-    x<-y
-  }
-  get<-function() x
-  setInv<-function(inverse) inv<<-inverse
-  getInv<-function() inv
-  list(get=get,set=set,setInv=setInv,getInv=getInv)
+    x<<-y
+    inv<<-NULL
+  }##It sets y to x and inv to NULL in parent environment
+  get<-function() x ##Gives input matrix
+  setInv<-function(inverse) inv<<-inverse ##sets inverse matrix that has given as input
+  getInv<-function() inv ##returns inverse of matrix value
+  list(get=get,set=set,setInv=setInv,getInv=getInv) ##It allows to call methods as elements
 }
 
-
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-  inv<-x$getInv()
+  inv<-x$getInv() ## Calls getInv() method defined in makeCacheMatrix function and assigns value to inv
   if(!is.null(inv)){
     message("getting cached data")
-  }
-  data<-x$get()
-  inv<-solve(data)
-  x$setInv(inv)
-  inv   ## Return a matrix that is the inverse of 'x'
+    return(inv)
+  }##returns message if it finds value for inv and returns it
+  data<-x$get() ##calls get method and returns input matrix
+  inv<-solve(data,...)##calculates inverse of matrix using solve() method
+  x$setInv(inv) ##calls setinv method and sets inverse
+  inv ## returns inv of matrix
 }
